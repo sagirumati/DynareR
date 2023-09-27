45fe1828ffcdf46d2e83b9799e73106020924f2c
sagirumati <smatiweb@gmail.com>
2023-09-27 22:04:58 +0300

    updated 2023-09-27

--- R/set_octave_path.R
+++ R/set_octave_path.R
@@ -13,4 +13,23 @@
 #' @family important functions
 #' @keywords documentation
 #' @export
-set_octave_path <- function(engine_path="octave") engine_path<<-engine_path
+set_octave_path <- function(engine_path="octave"){
+  engine_path=Sys.which(engine_path)
+
+  octave_version=c('7.3.0','8.1.0')
+  C:/Program Files/GNU Octave/Octave-7.3.0/mingw64/bin/octave.exe
+
+  if(engine_path=="") engine_path=Sys.which("EViews13_x64")
+  if(engine_path=="") engine_path=Sys.which("EViews13_x86")
+  if(engine_path=="") engine_path=Sys.which("EViews12_x64")
+  if(engine_path=="") engine_path=Sys.which("EViews12_x86")
+  if(engine_path=="") engine_path=Sys.which("EViews11_x64")
+  if(engine_path=="") engine_path=Sys.which("EViews11_x86")
+  if(engine_path=="") engine_path=Sys.which("EViews10_x64")
+  if(engine_path=="") engine_path=Sys.which("EViews10_x86")
+  if(engine_path=="") engine_path=Sys.which("EViews9_x64")
+  if(engine_path=="") engine_path=Sys.which("EViews9_x86")
+  if(engine_path=="") engine_path=Sys.which("EViews10")
+  if(engine_path=="") stop("EViews executable cannot be found, please use 'set_eviews_path' function to set the path to the EViews executable")
+  engine_path<<-engine_path
+}
